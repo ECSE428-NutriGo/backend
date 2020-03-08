c62db78a4520e32b3b47e628c2bf7d51e7d91135
Watem <watem.main@gmail.com>
2020-03-07 21:44:32 -0500

    finished edit food item tests

--- features/edit_food_item_attributes.feature
+++ features/edit_food_item_attributes.feature
@@ -7,16 +7,17 @@ So that the food item will have up-to-date information.
 Scenario: Edit Self-Created Food Item Attributes (Normal Flow)
 
 Given NutriGo user is logged into the application
+And there is a food item created by that user
 When the user enters valid attributes
-And the food item was created by that user
-And the user requests to edit a food item’s attributes
+And the user requests to edit that food item’s attributes
 Then the system remembers the updated food attributes
+And the response should be a success
 
 Scenario: Edit Other's Food Item Attributes (Error Flow)
 
 Given NutriGo user is logged into the application
+And there is a food item that is not created by that user
 When the user enters valid attributes
-But the food item was not created by that user
-And the user requests to edit a food item’s attributes
+And the user requests to edit that food item’s attributes
 Then the system does not allow the user to edit the attributes
 And the user should see an error message
--- features/steps/edit_food_item_steps.py
+++ features/steps/edit_food_item_steps.py
@@ -7,18 +7,76 @@ from django.contrib.auth.models import User
 from nutrition import controller
 from nutrition.models import Meal, MealEntry, FoodItem
 
-# import pdb
+import pdb
 
-
-@when('the user requests to edit a food item’s attributes')
+@given('there is a food item created by that user')
 def step_impl(context):
+    name1 = "name1"
+    protein1 = 1
+    fat1 = 1
+    carb1 = 1
+
+    url = '/nutri/fooditem/'
+    factory = APIRequestFactory()
+    view = controller.FoodItemController.as_view()
+
+    request = factory.post(
+        url,
+        json.dumps({
+            "name": name1,
+            "protein": protein1,
+            "fat": fat1,
+            "carb": carb1
+        }),
+        content_type='application/json'
+    )
+    force_authenticate(request, user=context.user)
+    context.fooditem_id = view(request).data['fooditem']['id']
+
+@given('there is a food item that is not created by that user')
+def step_impl(context):
+    anotherUser = User.objects.create_user(username="testB", email="testB@email.com")
+
+    name1 = "name1"
+    protein1 = 1
+    fat1 = 1
+    carb1 = 1
+
+    url = '/nutri/fooditem/'
+    factory = APIRequestFactory()
+    view = controller.FoodItemController.as_view()
+
+    request = factory.post(
+        url,
+        json.dumps({
+            "name": name1,
+            "protein": protein1,
+            "fat": fat1,
+            "carb": carb1
+        }),
+        content_type='application/json'
+    )
+    force_authenticate(request, user=anotherUser)
+    context.fooditem_id = view(request).data['fooditem']['id']
+
+
+@when('the user requests to edit that food item’s attributes')
+def step_impl(context):
+    url = '/nutri/fooditem/'
+    factory = APIRequestFactory()
+    view = controller.FoodItemController.as_view()
+    request = factory.get(url)
+    force_authenticate(request, user=context.user)
+    response = view(request)
+    fooditem = response.data['fooditems'][0]
+
     url = '/nutri/fooditem/'
     factory = APIRequestFactory()
     view = controller.FoodItemController.as_view()
     request = factory.put(
         url,
         json.dumps({
-            "fooditem_id": context.FoodItem.id,
+            "fooditem": context.fooditem_id,
             "name": "name2",
             "protein": 2,
             "fat": 2,
@@ -28,37 +86,49 @@ def step_impl(context):
     )
     force_authenticate(request, user=context.user)
     context.response = view(request)
-    # breakpoint()
 
-@when('the food item was created by that user')
-def step_impl(context):
-    context.FoodItem = FoodItem.objects.create(user=context.user, protein=1, fat=1, carb=1, name="created by user")
 @when('the user enters valid attributes')
 def step_impl(context):
+    url = '/nutri/fooditem/'
+    factory = APIRequestFactory()
+    view = controller.FoodItemController.as_view()
+    request = factory.get(url)
+    force_authenticate(request, user=context.user)
+    response = view(request)
+
     context.fooditem_name="name2"
     context.protein2=2
     context.fat2=2
     context.carb2=2
 
-@when('the food item was not created by that user')
-def step_impl(context):
-    anotherUser = User.objects.create_user(username="testB", email="testB@email.com")
-    context.FoodItem = FoodItem.objects.create(user=anotherUser, protein=1, fat=1, carb=1, name="created by another user")
 @then('the system remembers the updated food attributes')
 def step_impl(context):
-    # assert context.response.status_code == 200
-    # breakpoint()
     fooditem = context.response.data['fooditem']
-    assert fooditem.name == "name2"
-    assert fooditem.fat==2
-    assert fooditem.carb==2
-    assert fooditem.protein==2
+    assert fooditem["name"] == "name2"
+    assert fooditem["fat"]==2
+    assert fooditem["carb"]==2
+    assert fooditem["protein"]==2
 
 @then('the system does not allow the user to edit the attributes')
 def step_impl(context):
 
-    fooditem = context.response.data['fooditem']
-    assert(fooditem.name== "name1")
-    assert(fooditem.fat== 1)
-    assert(fooditem.carb== 1)
-    assert(fooditem.protein== 1)
+    url = '/nutri/fooditem/'
+    factory = APIRequestFactory()
+    view = controller.FoodItemController.as_view()
+    request = factory.get(url)
+    force_authenticate(request, user=context.user)
+    response = view(request)
+    fooditem = response.data['fooditems'][0]
+
+    assert fooditem["name"] == "name1"
+    assert fooditem["fat"] == 1
+    assert fooditem["carb"] == 1
+    assert fooditem["protein"] == 1
+
+@then('the response should be a success')
+def step_impl(context):
+    assert context.response.status_code == 200
+
+@then('the user should see an error message')
+def step_impl(context):
+    assert context.response.status_code >= 300
