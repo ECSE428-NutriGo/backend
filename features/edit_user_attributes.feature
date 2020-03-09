32da3838b87c610c36337663cf29fb5b62bcbfa8
Dylan Havelock <dylan.havelock@gmail.com>
2020-03-09 15:09:11 -0400

    added data tables

--- features/edit_user_attributes.feature
+++ features/edit_user_attributes.feature
@@ -4,25 +4,44 @@ As a user,
 I would like to be able to change my profile attributes,
 So that the system is up to date with all my contact and personal information.
 
-Scenario: Edit All User Attributes (Normal Flow)
+Scenario Outline: Edit All User Attributes (Normal Flow)
 
 Given NutriGo user is logged into the application
-When the user enters valid information for all changing attributes
+When the user enters valid "<email>", "<age>", "<hours_activity>", "<protein_target>", "<fat_target>", "<carb_target>" for all changing attributes
 And the user requests to edit the attributes of their profile
 Then the system will register the new user attributes for the user
 And the user should see a confirmation message
 
-Scenario: Edit Some User Attributes (Alternate Flow)
+Examples: Valid Information
+   | email         | age         | hours_activity   | protein_target    | fat_target    | carb_target   |
+   | test@user.com | 25          | 20               | 100               | 101           | 102           |
+
+Scenario Outline: Edit Some User Attributes (Alternate Flow)
 
 Given NutriGo user is logged into the application
-When the user enters some valid information for changing attributes and leaves other attributes blank
+When the user enters some valid "<email>", "<age>", "<hours_activity>", "<protein_target>", "<fat_target>", "<carb_target>" for changing attributes and leaves other attributes blank
 And the user requests to edit the attributes of their profile
 Then the system will register the new user attributes for the ones changed and will keep the old attributes for the fields left blank
 And the user should see a confirmation message
 
-Scenario: Invalid User Attributes (Error Flow)
+Examples: Partial Information
+   | email         | age         | hours_activity   | protein_target    | fat_target    | carb_target   |
+   | test@user.com | 25          | .                | 100               | 101           | 102           |
+   | test@user.com | 25          | 20               | .                 | 101           | 102           |
+   | test@user.com | 20          | 20               | 10                | .             | .             |
+   | test@user.com | 22          | .                | .                 | .             | .             |
+
+Scenario Outline: Invalid User Attributes (Error Flow)
 
 Given NutriGo user is logged into the application
-When the user enters invalid information for a given attributes
+When the user enters invalid "<email>", "<age>", "<hours_activity>", "<protein_target>", "<fat_target>", "<carb_target>" for a given attributes
 And the user requests to edit the attributes of their profile
 Then the system will not register the new user attributes for the invalid data
+
+Examples: Invalid Information
+   | email         | age         | hours_activity   | protein_target    | fat_target    | carb_target   |
+   | test@user.com | -1          | 20               | 100               | 101           | 102           |
+   | test@user.com | 25          | -1               | 100               | 101           | 102           |
+   | test@user.com | 20          | 20               | -1                | -1            | -1            |
+   | test@user.com | 20          | 20               | 100               | -2            | 102           |
+   | test@user.com | 20          | 20               | 100               | -2            | -2            |
--- features/steps/user_attributes_steps.py
+++ features/steps/user_attributes_steps.py
@@ -1,4 +1,5 @@
 from behave import *
+import parse
 
 from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate, APIClient
 from rest_framework.utils import json
@@ -14,14 +15,14 @@ def step_impl(context):
     context.response = client.put('/rest-auth/user/', context.json_request, format='json')
 
 
-@when('the user enters valid information for all changing attributes')
-def step_impl(context):
-    context.email = "validEmail@email.com"
-    context.age = 25
-    context.hours_activity = 20
-    context.protein_target = 100
-    context.fat_target = 101
-    context.carb_target = 102
+@when('the user enters valid "{email}", "{age}", "{hours_activity}", "{protein_target}", "{fat_target}", "{carb_target}" for all changing attributes')
+def step_impl(context, email, age, hours_activity, protein_target, fat_target, carb_target):
+    context.email = email
+    context.age = int(age)
+    context.hours_activity = int(hours_activity)
+    context.protein_target = int(protein_target)
+    context.fat_target = int(fat_target)
+    context.carb_target = int(carb_target)
 
     context.json_request = {
         "age": context.age,
@@ -31,22 +32,56 @@ def step_impl(context):
         "carb_target": context.carb_target 
     }
 
-@when('the user enters some valid information for changing attributes and leaves other attributes blank')
-def step_impl(context):
-    context.hours_activity = 20
-    context.carb_target = 102
+@when('the user enters some valid "{email}", "{age}", "{hours_activity}", "{protein_target}", "{fat_target}", "{carb_target}" for changing attributes and leaves other attributes blank')
+def step_impl(context, email, age, hours_activity, protein_target, fat_target, carb_target):
+    try:
+        context.age = int(age)
+    except ValueError:
+        context.age = None
+    try:
+        context.hours_activity = int(hours_activity)
+    except ValueError:
+        context.hours_activity = None
+    try:
+        context.protein_target = int(protein_target)
+    except ValueError:
+        context.protein_target = None
+    try:
+        context.fat_target = int(fat_target)
+    except ValueError:
+        context.fat_target = None
+    try:
+        context.carb_target = int(carb_target)
+    except ValueError:
+        context.carb_target = None
 
-    context.json_request = {
-        "hours_activity": context.hours_activity,
-        "carb_target": context.carb_target 
-    }
+    context.json_request = {}
+    if context.age is not None:
+        context.json_request["age"] = context.age
+    if context.hours_activity is not None:
+        context.json_request["hours_activity"] = context.hours_activity
+    if context.protein_target is not None:
+        context.json_request["protein_target"] = context.protein_target
+    if context.fat_target is not None:
+        context.json_request["fat_target"] = context.fat_target
+    if context.carb_target is not None:
+        context.json_request["carb_target"] = context.carb_target
 
-@when('the user enters invalid information for a given attributes')
-def step_impl(context):
-    context.age = -1
+@when('the user enters invalid "{email}", "{age}", "{hours_activity}", "{protein_target}", "{fat_target}", "{carb_target}" for a given attributes')
+def step_impl(context, email, age, hours_activity, protein_target, fat_target, carb_target):
+    context.email = email
+    context.age = int(age)
+    context.hours_activity = int(hours_activity)
+    context.protein_target = int(protein_target)
+    context.fat_target = int(fat_target)
+    context.carb_target = int(carb_target)
 
     context.json_request = {
         "age": context.age,
+        "hours_activity": context.hours_activity,
+        "protein_target": context.protein_target,
+        "fat_target": context.fat_target,
+        "carb_target": context.carb_target 
     }
 
 @then('the system will register the new user attributes for the user')
@@ -59,9 +94,45 @@ def step_impl(context):
 
 @then('the system will register the new user attributes for the ones changed and will keep the old attributes for the fields left blank')
 def step_impl(context):
-    assert context.response.data['hours_activity'] == context.hours_activity
-    assert context.response.data['carb_target'] == context.carb_target
+    if context.age:
+        assert context.response.data['age'] == context.age
+
+    if context.hours_activity:
+        assert context.response.data['hours_activity'] == context.hours_activity
+
+    if context.protein_target:
+        assert context.response.data['protein_target'] == context.protein_target
+
+    if context.fat_target:
+        assert context.response.data['fat_target'] == context.fat_target
+
+    if context.carb_target:
+        assert context.response.data['carb_target'] == context.carb_target
 
 @then('the system will not register the new user attributes for the invalid data')
 def step_impl(context):
-    assert context.response.data['age'] != context.age
+    if context.age < 0:
+        assert context.response.data['age'] != context.age
+    else:
+        assert context.response.data['age'] == context.age
+
+    if context.hours_activity < 0:
+        assert context.response.data['hours_activity'] != context.hours_activity
+    else:
+        assert context.response.data['hours_activity'] == context.hours_activity
+
+    if context.protein_target < 0:
+        assert context.response.data['protein_target'] != context.protein_target
+    else:
+        assert context.response.data['protein_target'] == context.protein_target
+
+    if context.fat_target < 0:
+        assert context.response.data['fat_target'] != context.fat_target
+    else:
+        assert context.response.data['fat_target'] == context.fat_target
+
+    if context.carb_target < 0:
+        assert context.response.data['carb_target'] != context.carb_target
+    else:
+        assert context.response.data['carb_target'] == context.carb_target
+
